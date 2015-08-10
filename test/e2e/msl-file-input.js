9ebe6cbf7f1a6b4e77af822bbec119c31d6d982b
marcoliceti <marco.liceti@gmail.com>
2015-08-10 18:02:10 +0200

    Added e2e Protractor tests

---
+++ test/e2e/msl-file-input.js
@@ -0,0 +1,37 @@
+describe('Directive msl-file-input', function() {
+	var path = require('path');
+
+	beforeEach(function() {
+    browser.get('http://localhost:8080/demo/msl-file-input/');
+  });
+
+	it('should append a hidden file input to the given container', function() {
+		var button = element(by.css('button'));
+		var hidden_input = button.element(by.css('input'));
+
+		expect(button.getText()).toEqual('Select files');
+		expect(hidden_input.isDisplayed()).toEqual(false);
+		expect(button.getAttribute('multiple')).toBeNull();
+		expect(hidden_input.getAttribute('multiple')).not.toBeNull();
+	});
+
+	it('should allow file selection', function() {
+		var button = element(by.css('button'));
+		var hidden_input = button.element(by.css('input'));
+		var files_list = element(by.css('ul'));
+		var files = files_list.all(by.css('li'));
+
+		expect(files_list.isPresent()).toEqual(true);
+		expect(files.count()).toEqual(0);
+
+		var a_file = path.resolve(__dirname, '../../demo/msl-file-input/index.html');
+		console.log(a_file);
+		browser.driver.executeScript(function () {
+			document.getElementsByTagName('input')[0].style.display = 'inline';
+		});
+		hidden_input.sendKeys(a_file);
+		expect(files.count()).toEqual(1);
+		expect(files.first().getText()).toEqual('index.html');
+	});
+
+});
